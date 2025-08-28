83e7089c89273369e333a72d37d5c4922b1833f7
Kusum Kumari <kusumkumariguraru@gmail.com>
2025-08-28 12:44:46 +0530

    Update MultiSignature-_Wallets.sol

--- Contracts/MultiSignature-_Wallets.sol
+++ Contracts/MultiSignature-_Wallets.sol
@@ -1,1 +1,148 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
+contract MultiSigWallet {
+    address[] public owners = [
+        0x5B38Da6a701c568545dCfcB03FcB875f56beddC4,
+        0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2,
+        0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
+    ];
+    
+    mapping(address => bool) public isOwner;
+    uint256 public required = 2;
+    
+    struct Transaction {
+        address to;
+        uint256 value;
+        bytes data;
+        bool executed;
+        uint256 confirmations;
+    }
+    
+    Transaction[] public transactions;
+    mapping(uint256 => mapping(address => bool)) public isConfirmed;
+    
+    event Deposit(address indexed sender, uint256 amount);
+    event Submit(uint256 indexed txIndex);
+    event Confirm(address indexed owner, uint256 indexed txIndex);
+    event Execute(uint256 indexed txIndex);
+    
+    constructor() {
+        for (uint256 i = 0; i < owners.length; i++) {
+            isOwner[owners[i]] = true;
+        }
+    }
+    
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "not owner");
+        _;
+    }
+    
+    modifier txExists(uint256 _txIndex) {
+        require(_txIndex < transactions.length, "tx does not exist");
+        _;
+    }
+    
+    modifier notExecuted(uint256 _txIndex) {
+        require(!transactions[_txIndex].executed, "tx already executed");
+        _;
+    }
+    
+    modifier notConfirmed(uint256 _txIndex) {
+        require(!isConfirmed[_txIndex][msg.sender], "tx already confirmed");
+        _;
+    }
+    
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value);
+    }
+    
+    function submit(address _to, uint256 _value, bytes memory _data) public onlyOwner {
+        uint256 txIndex = transactions.length;
+        
+        transactions.push(Transaction({
+            to: _to,
+            value: _value,
+            data: _data,
+            executed: false,
+            confirmations: 0
+        }));
+        
+        emit Submit(txIndex);
+    }
+    
+    function confirm(uint256 _txIndex)
+        public
+        onlyOwner
+        txExists(_txIndex)
+        notExecuted(_txIndex)
+        notConfirmed(_txIndex)
+    {
+        Transaction storage transaction = transactions[_txIndex];
+        transaction.confirmations += 1;
+        isConfirmed[_txIndex][msg.sender] = true;
+        
+        emit Confirm(msg.sender, _txIndex);
+    }
+    
+    function execute(uint256 _txIndex)
+        public
+        onlyOwner
+        txExists(_txIndex)
+        notExecuted(_txIndex)
+    {
+        Transaction storage transaction = transactions[_txIndex];
+        
+        require(transaction.confirmations >= required, "not enough confirmations");
+        
+        transaction.executed = true;
+        
+        (bool success, ) = transaction.to.call{value: transaction.value}(transaction.data);
+        require(success, "tx failed");
+        
+        emit Execute(_txIndex);
+    }
+    
+    function revoke(uint256 _txIndex)
+        public
+        onlyOwner
+        txExists(_txIndex)
+        notExecuted(_txIndex)
+    {
+        require(isConfirmed[_txIndex][msg.sender], "tx not confirmed");
+        
+        Transaction storage transaction = transactions[_txIndex];
+        transaction.confirmations -= 1;
+        isConfirmed[_txIndex][msg.sender] = false;
+    }
+    
+    function getOwners() public view returns (address[] memory) {
+        return owners;
+    }
+    
+    function getTransactionCount() public view returns (uint256) {
+        return transactions.length;
+    }
+    
+    function getTransaction(uint256 _txIndex)
+        public
+        view
+        returns (
+            address to,
+            uint256 value,
+            bytes memory data,
+            bool executed,
+            uint256 confirmations
+        )
+    {
+        Transaction storage transaction = transactions[_txIndex];
+        
+        return (
+            transaction.to,
+            transaction.value,
+            transaction.data,
+            transaction.executed,
+            transaction.confirmations
+        );
+    }
+}
